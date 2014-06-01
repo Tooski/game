7ac3e483d1bee868962492c9bfd353c5b1643452
min0428 <min0428@uw.edu>
2014-05-31 18:29:43 -0700

    game over functionupdated
    updated with it..

--- public_html/gameOver.js
+++ public_html/gameOver.js
@@ -1,6 +1,15 @@
 function gameOver(the_score, the_time, the_jason, the_collect, the_num_death){
 	
-	// insert to thing..
+	
+	
+	
+	
+	
+	// insert to to database.
+	game.settings.post({"command": "insertScore",
+            "data": {"userID": my_user_name, "levelID": my_g_level_id,
+                "score": the_score, "completetime": the_time,"replay":the_jason}}, 
 	//display..it.
+	displayLeaderBoard();
 	
 }
