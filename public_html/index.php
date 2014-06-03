6e3c42c143194de455d14e19831cdc20f7211840
Mike <mike77@uw.edu>
2014-06-02 17:45:02 -0700

    switched off the initial options

--- public_html/index.php
+++ public_html/index.php
@@ -86,7 +86,7 @@
         </div>
 
         <!--div for initial choice to login or sign up-->
-        <div class="outer-signup-container" id="choice_board" style= "display : block">
+        <div class="outer-signup-container" id="choice_board" style= "display : none">
             <div class="inner-header-container">
 
             </div>
